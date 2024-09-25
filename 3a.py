1a09ec8d76627774e23f45529b241f2baba213bf
VachanBaleagr <vachanbalegar@gmail.com>
2024-09-25 00:33:17 -0700

    Add files via upload

---
+++ 3a.py
@@ -0,0 +1,18 @@
+S=input("Enter a sentence");
+w=0
+d=0
+u=0
+l=0
+r= S.split()
+w=len(r)
+for i in S:
+    if i.isdigit():
+        d=d+1
+    elif i.isupper():
+        u=u+1
+    else:
+        l=l+1
+print("Number of words:",w)
+print("Number of digits:",d)
+print("Number of uppercase:",u)
+print("Number of lowercase:",l)

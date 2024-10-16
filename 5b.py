1006c3d30a2500744bc048ddc16e32ce7d7fce52
VachanBaleagr <vachanbalegar@gmail.com>
2024-10-16 00:22:22 -0700

    Add files via upload

---
+++ 5b.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+plt.hist([35,72,85,62,88,96,98,64,55,18,24,36],bins=4)
+plt.xlabel("Distribution of age",fontsize=16)
+plt.ylabel("Frequency",fontsize=12)
+plt.title("Vachan-1KI23CS173")
+plt.show()

e1975aedef5605d4961ccef917374d9aec820751
VachanBaleagr <vachanbalegar@gmail.com>
2024-10-22 23:22:46 -0700

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+CARS=['Ferrai','Tesla','Jaguar','MarutiSuzuki','Toyata','RR']
+data=['13','18','16','20','19','14']
+plt.figure(figsize=(10,7))
+plt.pie(data,labels=CARS)
+plt.title("Vachan-1KI23CS173")
+plt.show()

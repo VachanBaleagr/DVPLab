43fe45af68db81577cb51f19f701d325bc285cca
VachanBaleagr <vachanbalegar@gmail.com>
2024-11-05 23:52:28 -0800

    Add files via upload

--- 6b.py
+++ 6b.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 overs=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
-Runs=[6,18,13,20,11,22,9,6,7,5,8,10,7,5,12,14,18,30,23,30]
+Runs=[6,12,18,24,30,36,42,48,54,60,66,72,78,84,90,96,102,108,114,120]
 plt.plot(overs,Runs,marker='x',markerfacecolor='blue',markersize=8,linestyle='dashed',linewidth=2,color='red')
 plt.xlabel("overs",color='black')
 plt.ylabel("Runs",color='red')
